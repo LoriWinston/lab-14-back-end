b33ce58295b753a7b4a25156de79dc88d1caa68d
Lori Winston <Lcwinston@gmail.com>
2021-03-04 20:51:28 -0800

    initial setup

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,12 +17,12 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE favorites (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    advice VARCHAR(512) NOT NULL,
+                    advice_id INTEGER NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
-            );
+                    );
         `);
 
     console.log('create tables complete', getEmoji(), getEmoji(), getEmoji());
---
+++ data/favorites.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    advice: 'have a nice day',
+    advice_id: 1,
+  },
+  {
+    advice: 'have an excellent weekend',
+    advice_id: 2,
+  },
+  {
+    advice: 'hey you, you are great',
+    advice_id: 3,
+  }
+];
+  
